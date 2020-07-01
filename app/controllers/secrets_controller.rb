7c370231d27a9b22dd1ef50b9a4065c1125171e5
iona-b <ionabrabender@gmail.com>
2020-06-30 20:04:31 -0700

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,25 @@
+class SecretsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name] == nil
+      redirect_to controller: 'sessions', action: 'new'
+    elsif params[:name] == ""
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      params[:name] != nil
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/login'
+  end
+
+  def current_user
+
+  end
+end
